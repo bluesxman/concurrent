ab737a2cd6e19d5dd662e88f7718d3ca6158c2b4
Jon Newton <jon.newton@gmail.com>
2014-01-13 09:46:32 -0700

    prime-seq 2x faster.  no more multi-map

--- src/concurrent/futures.clj
+++ src/concurrent/futures.clj
@@ -1,10 +1,9 @@
 (ns concurrent.futures
   (:require [concurrent.primes :refer :all]))
 
+(time (apply + (take-while #(< % 2e6) (prime-seq))))
 
-(time (apply + (take-while #(< % 2e6) (primes/prime-seq))))
-
-(time (println (map deref (map #(future (nth (primes/prime-seq) (* % 3e5))) [1 1 1 1]))))
-(time (println (let [p (primes/prime-seq)] (map deref (map #(future (nth p (* % 3e5))) [1 1 1 1])))))
-(time (println (map #(nth (primes/prime-seq) (* % 3e5)) [1 1 1 1])))
+(time (println (map deref (map #(future (nth (prime-seq) (* % 1e5))) [1 1 1 1]))))
+(time (println (let [p (prime-seq)] (map deref (map #(future (nth p (* % 1e5))) [1 1 1 1])))))
+(time (println (map #(nth (prime-seq) (* % 1e5)) [1 1 1 1])))
 
